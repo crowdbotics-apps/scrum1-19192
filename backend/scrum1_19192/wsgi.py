4b9c5fc015a9f6fcc171cd6d26529511350ea8e4
Crowdbotics <team@crowdbotics.com>
2020-07-26 15:05:53 +0000

    Update models

--- backend/scrum1_19192/wsgi.py
+++ backend/scrum1_19192/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrum1_19192.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrum1_19192.settings")
 
 application = get_wsgi_application()
